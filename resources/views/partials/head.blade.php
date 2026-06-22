8ca11f1889eb6e17b601e6136bd0ff463906e8d2
yudsetiwann <yudsetiwann@gmail.com>
2026-06-23 03:51:18 +0700

    Change the icon on the browser tab

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,9 +5,8 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="/img/logo.png" type="image/png">
+<link rel="apple-touch-icon" href="/img/logo.png">
 
 @fonts
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Booster Coffee') }} &mdash; Point of Sale</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/img/logo.png" type="image/png">
+    <link rel="apple-touch-icon" href="/img/logo.png">
 
     @fonts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
